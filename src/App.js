4b8cc072be69e05c3c8c3fad4b33c09af7536aa7
Brandon Phillips <brandon.phillips976@gmail.com>
2021-02-12 16:51:35 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHomeLgAlt } from '@fortawesome/pro-solid-svg-icons'
+import { faSearch } from '@fortawesome/pro-regular-svg-icons'
+import './index.css'
+
+console.log(FontAwesomeIcon)
+
+export default function Header() {
+    const searchInput = useRef()
+    const searchInputWrapper = useRef()
+
+    const [isFocusedOnSearch, setIsFocusedOnSearch] = useState(false)
+
+    const handleSearchFormSubmit = (e) => {
+        e.preventDefault()
+    }  
+
+    return (
+        <header>
+            <div className='header-flex'>
+                <div className='flex-item-group'>
+                    <Link to='/' className='brand'><h1>Title Goes Here</h1></Link>
+                </div>
+                <div className='flex-item-group'>
+                    <form ref={searchInputWrapper}
+                        className={`header-search-wrapper${isFocusedOnSearch ? ' focused' : ''}`}
+                        onSubmit={handleSearchFormSubmit}
+                    >
+                        <input ref={searchInput}
+                            className='search-bar'
+                            placeholder='search'
+                            onFocus={() => setIsFocusedOnSearch(true)}
+                            onBlur={() => setIsFocusedOnSearch(false)}
+                        />
+                        <button className='search-icon-btn'>
+                            <FontAwesomeIcon icon={faSearch}/>
+                        </button>
+                    </form>
+                    <div className='search-results-wrapper'></div>
+                </div>
+                <div className='flex-item-group link-icons'>
+                    <Link to='/' aria-label='home' className='nav-link'>
+                        <FontAwesomeIcon icon={faHomeLgAlt} />
+                    </Link>
+                    <Link to='#' aria-label='messages' className='nav-link'>
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </Link>
+                    <Link to='#' aria-label='messages' className='nav-link'>
+                        <FontAwesomeIcon icon={faUser}/>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
+}
